7abdb1b711b1ef43422bb1047e78276aed53c2ba
rediet tesfaye <rediettesfayefetenu@gmail.com>
2026-05-20 18:51:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function viewEvent(){
+    window.location.href = "event.html";
+}
+
+function register(){
+    alert("Registered Successfully!");
+}
+
+function login(){
+    window.location.href = "dashboard.html";
+}
